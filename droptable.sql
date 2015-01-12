b5f463fdb8343fd669778d31288f028a1df1e762
searph <junchili@usc.edu>
2015-01-12 11:01:00 -0800

    Create droptable.sql

---
+++ droptable.sql
@@ -0,0 +1,8 @@
+DELETE FROM user_sdo_geom_metadata;
+drop index as1_idx;
+drop index as2_idx;
+drop index building_idx;
+drop index student_idx;
+drop table student;
+drop table building;
+drop table announcementsystem;
